aa7e75086534f43096e599bb9ee6c9a6f052e79c
ian.hellozin <ian.hellozin@kakaocorp.com>
2020-04-12 20:29:38 +0900

    [PS][ADD] : 완주하지 못한 선수

---
+++ src/me/hellozin/ps/programmers/hash/RetiredPlayer.java
@@ -0,0 +1,35 @@
+package me.hellozin.ps.programmers.hash;
+
+import java.util.Arrays;
+
+/*
+* 제목 : 완주하지 못한 선수
+* 링크 : https://programmers.co.kr/learn/courses/30/lessons/42576
+* */
+
+public class RetiredPlayer {
+
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+        String expected = "leo";
+        Solution solution = new Solution();
+        String actual = solution.solution(participant, completion);
+        System.out.println("result : " + expected.equals(actual));
+    }
+
+}
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+        return participant[participant.length-1];
+    }
+}
